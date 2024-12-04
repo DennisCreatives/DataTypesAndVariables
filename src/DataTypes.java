ef9354b6f714df3a9914ab97d3950099f1612130
DennisCreatives <creatives.dennis@gmail.com>
2024-12-04 15:01:08 +0300

    first commit

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
-
 public class DataTypes {
+    public static void main(String[] args) {
+        // Comments
+        // 1. This is a single line comment
+
+    /*
+    2. This is a
+    multiple line
+    comment */
+
+        /**
+         * 3. This is a special javadoc comment
+         */
+
+        // Variables in Java
+
+        // In Java, variables are used to store data values. Before a variable can be used, it must be declared with a specific data type and an identifier (name).
+        // Here's the syntax for variable declaration:
+        // dataType variableName;
+
+        // Example
+        int userAge;
+
+        // After creating a variable you can assign a value to it using the assignment (=) operator.
+        userAge = 18;
+
+        // You can do the declaration and initialization on the same line
+        int userScore = 90;
+
+        // Data Types in Java
+        // There are two categories of data types in Java: Primitive data types and Reference data types
+
+        // A. Primitive Data Types
+        // In Java, there are eight primitive data types, which are the basic building blocks for storing abd manipulating data.
+
+        // 1. Byte
+        // The byte data type is an 8-bit signed integer that can store value from -128 to 127
+
+        // 2. Short
+        // The short data type is a 16-bit signed integer that can store values from -32,768 to 32,767
+
+        // 3. Integer (int)
+        // The int data type is a 32-bit signed integer that can store values from -2,147,483,648 to 2,147,483,647
+
+        // 4. Long
+        // The long data type is a 64-bit signed integer that can store values from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        // 5. Float
+        // The float data type is a 32-bit single-precision floating-point number that can store up to approximately 7 decimal digits
+
+        // 6. Double
+        // The double data type is 64-bit double-precision floating-point number that can store numbers with a higher degree of precision compared to float data type.
+
+        // 7. Boolean
+        // A boolean data type represents two states - true and false
+
+        // 8. Character
+        // A character data type represents a single unicode character, enclosed in between single quotation marks
+    }
 }
